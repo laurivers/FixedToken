3463357018371a562897fdb975558c130538d333
LiuKang <lau.rivers@gmail.com>
2021-10-08 20:48:39 +0800

    修改合约

--- contracts/ERC20FixedSupply.sol
+++ contracts/ERC20FixedSupply.sol
@@ -3,7 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20FixedSupply is ERC20 {
-    constructor() public ERC20("Fixed", "FIX") {
-        _mint(msg.sender, 1.5e9);
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) public ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var ERC20FixedSupply = artifacts.require("ERC20FixedSupply");
+const ERC20FixedSupply = artifacts.require("ERC20FixedSupply");
 
-module.exports = function(deployer) {
-  deployer.deploy(ERC20FixedSupply);
+module.exports = async function (deployer) {
+  await deployer.deploy(ERC20FixedSupply, 'FixedToken', 'FIX', '10000000000000000000000');
 };
