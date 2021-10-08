122a6a5e289100c3c3eb23073456fc97297e7a8f
LiuKang <lau.rivers@gmail.com>
2021-10-08 17:43:48 +0800

    修改脚本

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 const Web3 = require("web3")
 const web3 = new Web3("http://localhost:7545")
+require('dotenv').config();
 
 
-console.log('Accounts', web3.eth.personal.getAccounts());
-console.log('password', process.env.ETH_HOLDER_WALLET_PWD)
-await web3.eth.personal.unlockAccount(accounts[0], process.env.ETH_HOLDER_WALLET_PWD);
-console.log('Account to load with ETH: ', accounts[0]);
+module.exports = async function () {
+    console.log('Accounts', web3.eth.personal.getAccounts());
+    // console.log('Accounts', accounts);
+    await web3.eth.personal.unlockAccount(accounts[0], process.env.ETH_HOLDER_WALLET_PWD);
+    console.log('Account to load with ETH: ', accounts[0]);
+};
