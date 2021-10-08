f36d05108a9638dbcabb79c13aff14fd4c12d815
LiuKang <lau.rivers@gmail.com>
2021-10-08 21:32:31 +0800

    修改

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,7 +4,7 @@ const Migrations = artifacts.require("Migrations");
 
 module.exports = async function (deployer, network, accounts) {
   // console.log('Accounts', accounts);
-  // await web3.eth.personal.unlockAccount(accounts[0], process.env.ETH_HOLDER_WALLET_PWD);
+  await web3.eth.personal.unlockAccount(accounts[0], 'JNUEbsq73nsMQfTB');
   // console.log('Account to load with ETH: ', accounts[0]);
   deployer.deploy(Migrations);
 };
