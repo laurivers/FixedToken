0fe0896059830a23d2a2acddb096943754755e66
LiuKang <lau.rivers@gmail.com>
2021-10-08 18:11:49 +0800

    修改配置

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,9 +4,9 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const Migrations = artifacts.require('./Migrations.sol');
 
 module.exports = async function (deployer, network, accounts) {
-  console.log('Accounts', accounts);
-  console.log('Password', process.env.ETH_HOLDER_WALLET_PWD);
-  await web3.eth.personal.unlockAccount(accounts[0], process.env.ETH_HOLDER_WALLET_PWD);
-  console.log('Account to load with ETH: ', accounts[0]);
+  // console.log('Accounts', accounts);
+  // console.log('Password', process.env.ETH_HOLDER_WALLET_PWD);
+  // await web3.eth.personal.unlockAccount(accounts[0], process.env.ETH_HOLDER_WALLET_PWD);
+  // console.log('Account to load with ETH: ', accounts[0]);
   await deployer.deploy(Migrations);
 };
